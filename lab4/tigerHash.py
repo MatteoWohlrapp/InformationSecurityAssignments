805048b4d4b7a41431a53ea3c2b67b20fb2a79cc
Lubor Budaj <l.budaj@student.rug.nl>
2021-10-19 00:46:26 +0200

    tiger fixed multiple by 8 padding & inner rounds schedule

--- lab4/tigerHash.py
+++ lab4/tigerHash.py
@@ -5,28 +5,21 @@ from operation import *
 UNIT = 64
 BIT = 8
 
-
 def read_input():
     return sys.stdin.buffer.read()
-
 
 def pad(text):
     padded_text = text + b'\x01'
     while len(padded_text) % UNIT != 56:
         padded_text += b'\x00'
-    return padded_text + (len(text)).to_bytes(8, 'big')
-
+    return padded_text + (8*len(text)).to_bytes(8, 'big')
 
 # w is 8 byte (64 bit)
 # w[i] is 1 byte (8 bit)
 def key_schedule(w):
     w[0] = and4(w[0] - and4(w[7] ^ 0xA5A5A5A5A5A5A5A5))
-    # w[0] = minus(w[0], xor(w[7], bt(0xA5A5A5A5A5A5A5A5)))
-    # w[1] = xor(w[1], w[0])
     w[1] = and4(w[1] ^ w[0])
-    # w[2] = plus(w[2], w[1])
     w[2] = and4(w[2] + w[1])
-    # w[3] = minus(w[3], xor(w[2], (~w[1]) << 19))
     w[3] = and4(w[3] - and4(w[2] ^ and4(~w[1] << 19)))
     w[4] = and4(w[4] ^ w[3])
     w[5] = and4(w[5] + w[4])
@@ -42,7 +35,6 @@ def key_schedule(w):
     w[7] = and4(w[7] - and4(w[6] ^ 0x0123456789ABCDEF))
     return w
 
-
 # a, b, c, w are 8 byte
 def inner_round(a, b, c, w, m):
     c ^= w
@@ -53,24 +45,18 @@ def inner_round(a, b, c, w, m):
     a &= 0xffffffffffffffff
     b &= 0xffffffffffffffff
     c &= 0xffffffffffffffff
-    # c = xor(c, w)
-    # a = minus(a, xor(xor(xor(bt(s0[c[0]]), bt(s1[c[2]])), bt(s2[c[4]])), bt(s3[c[6]])))
-    # b = plus(b, xor(xor(xor(bt(s3[c[1]]), bt(s2[c[3]])), bt(s1[c[5]])), bt(s0[c[7]])))
-    # b = multi(b, m)
     return a, b, c
-
 
 def inner_rounds(a, b, c, W, m):
     a, b, c = inner_round(a, b, c, W[0], m)
     a, b, c = inner_round(b, c, a, W[1], m)
-    a, b, c = inner_round(b, c, a, W[2], m)
-    a, b, c = inner_round(b, c, a, W[3], m)
+    a, b, c = inner_round(c, a, b, W[2], m)
+    a, b, c = inner_round(a, b, c, W[3], m)
     a, b, c = inner_round(b, c, a, W[4], m)
-    a, b, c = inner_round(b, c, a, W[5], m)
-    a, b, c = inner_round(b, c, a, W[6], m)
+    a, b, c = inner_round(c, a, b, W[5], m)
+    a, b, c = inner_round(a, b, c, W[6], m)
     a, b, c = inner_round(b, c, a, W[7], m)
     return c, a, b
-
 
 def outer_rounds(W, a, b, c):
     # a, b, c = 0x0123456789ABCDEF, 0xFEDCBA9876543210, 0xF096A5B4C3B2E187
@@ -90,15 +76,12 @@ def outer_rounds(W, a, b, c):
     c = (c + cc) & F
     return a, b, c
 
-
 def tiger_hash(X):
     a, b, c = 0x0123456789ABCDEF, 0xFEDCBA9876543210, 0xF096A5B4C3B2E187
     for x in X:
         W = [int.from_bytes(x[i:i + 8], 'big') for i in range(0, len(x), 8)]
-        print(W)
         a, b, c = outer_rounds(W, a, b, c)
     return a, b, c
-
 
 def main():
     text = read_input()
@@ -106,9 +89,8 @@ def main():
     X = [padded_text[i:i + UNIT] for i in range(0, len(padded_text), UNIT)]  # each 512 bits (64 bytes)
     a,b,c = tiger_hash(X)
     hash = a.to_bytes(8, 'big') + b.to_bytes(8, 'big') + c.to_bytes(8, 'big')
-    print(hash)
     sys.stdout.buffer.write(hash)
-
+    #sys.stdout.buffer.write(padded_text)
 
 if __name__ == '__main__':
     main()
